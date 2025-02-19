9d7a10c2758d5d2a2f0051351636e8623b818666
payalkhawase <payal.khawase14@gmail.com>
2025-02-19 12:09:02 +0530

    add eureka server

---
+++ EurekaServer/src/test/java/in/shriram/eurekaeerver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.shriram.eurekaeerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
