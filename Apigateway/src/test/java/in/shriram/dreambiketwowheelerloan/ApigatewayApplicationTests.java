9c36d926970ce9459a37f75de67428408daeb3a4
priyankabagde2610 <priyankabagde2610@gmail.com>
2025-02-20 13:01:17 +0530

    add apigateway

--- Apigateway/src/test/java/in/shriram/dreambiketwowheelerloan/ApigatewayApplicationTests.java
+++ Apigateway/src/test/java/in/shriram/dreambiketwowheelerloan/ApigatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package in.shriram.apigateway;
+package in.shriram.dreambiketwowheelerloan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class ApigatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
